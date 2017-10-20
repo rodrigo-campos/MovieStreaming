00e5af7aed0b1d77a923d0d87d3578eb60e0769e
Rodrigo Campos <rod.apd@gmail.com>
2017-10-20 08:28:34 -0200

    Add should handle predicate example

--- MovieStreaming/Actors/PlaybackActor.cs
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -14,7 +14,7 @@ namespace MovieStreaming.Actors
         {
             Console.WriteLine("Creating a PlaybackActor");
 
-            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message));
+            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message), message => message.UserId == 42);
         }
 
         private void HandlePlayMovieMessage(PlayMovieMessage message)
