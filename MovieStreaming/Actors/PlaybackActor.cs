1ba7e75666e2e4542745592992ccc671f70fe6b8
Rodrigo Campos <rod.apd@gmail.com>
2017-10-20 09:04:08 -0200

    Add lifecycle hooks

--- MovieStreaming/Actors/PlaybackActor.cs
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -21,5 +21,29 @@ namespace MovieStreaming.Actors
         {
             ColorConsole.WriteLineYellow($"PlayMovieMessage '{message.MovieTitle}' for user {message.UserId}");
         }
+
+        protected override void PreStart()
+        {
+            ColorConsole.WriteLineGreen("PlayBackActor PreStart");
+        }
+
+        protected override void PostStop()
+        {
+            ColorConsole.WriteLineGreen("PlayBackActor PostStop");
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            ColorConsole.WriteLineGreen($"PlayBackActor PreRestart because: {reason}");
+
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            ColorConsole.WriteLineGreen($"PlayBackActor PostRestart because: {reason}");
+
+            base.PostRestart(reason);
+        }
     }
 }
