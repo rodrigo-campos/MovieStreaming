713e621574d24f0e6c3142152b87ef77c4e92355
Rodrigo Campos <rod.apd@gmail.com>
2017-10-20 08:08:00 -0200

    First commit

---
+++ MovieStreaming/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using MovieStreaming.Actors;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create(nameof(MovieStreamingActorSystem));
+            Console.WriteLine("Actor system created");
+
+            Props playbackActorProps = Props.Create<PlaybackActor>();
+
+            IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            Console.ReadLine();
+
+            MovieStreamingActorSystem.Terminate();
+        }
+    }
+}
